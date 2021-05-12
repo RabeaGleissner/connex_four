1598e5af7296bf57e7b69930a82c204541310d38
Rabea Gleissner <rabeagleissner@gmail.com>
2021-05-12 14:03:02 +0800

    Figure out winner for two moves
    Co-authored-by: Craig Ambrose <craig@craigambrose.com>

--- lib/connect_four.ex
+++ lib/connect_four.ex
@@ -1,5 +1,17 @@
 defmodule ConnectFour do
-  def game_over?(_) do
-    false
+
+  def won?([moves: moves, config: [size: size, connect_what: connect_what]]) do
+    most_contiguous_moves(moves) >= connect_what
+  end
+
+  defp most_contiguous_moves([]), do: 0
+  defp most_contiguous_moves([_]), do: 1
+  defp most_contiguous_moves(moves) do
+    [{_, {first_move_x, _}}, {_, {second_move_x, _}}] = moves
+    if first_move_x + 1 == second_move_x do
+      2
+    else
+      1
+    end
   end
 end
--- test/connect_four_test.exs
+++ test/connect_four_test.exs
@@ -2,24 +2,37 @@ defmodule ConnectFourTest do
   use ExUnit.Case
   doctest ConnectFour
 
-  #[
-    #[:empty, :empty, :empty, :empty, :empty, :empty, :empty],
-    #[:empty, :empty, :empty, :empty, :empty, :empty, :empty],
-    #[:empty, :empty, :empty, :empty, :two, :empty, :empty],
-    #[:empty, :empty, :empty, :one, :one, :empty, :empty],
-    #[:one, :two, :one, :one, :one, :two, :two],
-    #[:one, :two, :two, :one, :one, :two, :two]
-  #]
+  describe "horizontal matching" do
+    test "game not won if there are no moves" do
+      game = [moves: [], config: [size: {4, 1}, connect_what: 4]]
+      refute ConnectFour.won?(game)
+    end
 
-  test "game is not over" do
-    game_state = [
-      [:empty, :empty, :empty, :empty, :empty, :empty, :empty],
-      [:empty, :empty, :empty, :empty, :empty, :empty, :empty],
-      [:empty, :empty, :empty, :empty, :empty, :empty, :empty],
-      [:empty, :empty, :empty, :empty, :empty, :empty, :empty],
-      [:empty, :empty, :empty, :empty, :empty, :empty, :empty],
-      [:empty, :empty, :empty, :empty, :empty, :empty, :empty],
-    ]
-    refute ConnectFour.game_over?(game_state)
+    test "game won if there is a single move for connect one" do
+      game = [moves: [{:one, {0, 0}}], config: [size: {4, 1}, connect_what: 1]]
+      assert ConnectFour.won?(game)
+    end
+
+    test "game not won for single move for connect two" do
+      game = [moves: [{:one, {0, 0}}], config: [size: {4, 2}, connect_what: 2]]
+      refute ConnectFour.won?(game)
+    end
+
+    test "game not won for two unconnected moves in connect two" do
+      game = [moves: [
+          {:one, {0, 0}},
+          {:one, {2, 0}}
+        ], config: [size: {4, 1}, connect_what: 2]]
+      refute ConnectFour.won?(game)
+    end
+
+    test "game won for two connected moves in connect two" do
+      game = [moves: [
+          {:one, {0, 0}},
+          {:one, {1, 0}}
+        ], config: [size: {4, 1}, connect_what: 2]]
+      assert ConnectFour.won?(game)
+    end
+
   end
 end
