44385535781aad89c65ac5267901601539285383
Rabea Gleissner <rabeagleissner@gmail.com>
2021-06-03 11:04:28 +0800

    Add horizontal matching logic

---
+++ lib/horizontal_matching.ex
@@ -0,0 +1,12 @@
+defmodule HorizontalMatching do
+  def contiguous_moves(start, coins, direction) do
+    if Enum.member?(coins, start) do
+      1 + contiguous_moves(next(start, direction), coins, direction)
+    else
+      0
+    end
+  end
+
+  defp next({x, y}, :positive), do: {x + 1, y}
+  defp next({x, y}, :negative), do: {x - 1, y}
+end
---
+++ test/horizontal_matching_test.exs
@@ -0,0 +1,41 @@
+defmodule HorizontalMatchingTest do
+  use ExUnit.Case
+  doctest HorizontalMatching
+
+  describe "contiguous horizontal moves" do
+    test "is zero if there are no coins" do
+      start = {}
+      coins = []
+      assert HorizontalMatching.contiguous_moves(start, coins, :positive) == 0
+      assert HorizontalMatching.contiguous_moves(start, coins, :negative) == 0
+    end
+
+    test "is zero if there is no coin on the start position" do
+      start = {2, 2}
+      coins = [{0, 0}]
+      assert HorizontalMatching.contiguous_moves(start, coins, :positive) == 0
+      assert HorizontalMatching.contiguous_moves(start, coins, :negative) == 0
+    end
+
+    test "is one if there is one move at the start position" do
+      start = {2, 2}
+      coins = [{0, 0}, {2, 2}]
+      assert HorizontalMatching.contiguous_moves(start, coins, :positive) == 1
+      assert HorizontalMatching.contiguous_moves(start, coins, :negative) == 1
+    end
+
+    test "is three if there are three contiguous moves in a positive direction" do
+      start = {2, 2}
+      coins = [{2, 2}, {3, 2}, {4, 2}]
+      assert HorizontalMatching.contiguous_moves(start, coins, :positive) == 3
+      assert HorizontalMatching.contiguous_moves(start, coins, :negative) == 1
+    end
+
+    test "is three if there are three contiguous moves in a negative direction" do
+      start = {2, 2}
+      coins = [{2, 2}, {1, 2}, {0, 2}]
+      assert HorizontalMatching.contiguous_moves(start, coins, :positive) == 1
+      assert HorizontalMatching.contiguous_moves(start, coins, :negative) == 3
+    end
+  end
+end
