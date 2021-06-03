1e51cc5db71cc7e782e986c619f5fedb4c2e3cdb
Rabea Gleissner <rabeagleissner@gmail.com>
2021-06-03 10:24:06 +0800

    Add vertical matching logic

---
+++ lib/vertical_matching.ex
@@ -0,0 +1,12 @@
+defmodule VerticalMatching do
+  def contiguous_moves(start, coins, direction) do
+    if Enum.member?(coins, start) do
+      1 + contiguous_moves(next(start, direction), coins, direction)
+    else
+      0
+    end
+  end
+
+  defp next({x, y}, :positive), do: {x, y + 1}
+  defp next({x, y}, :negative), do: {x, y - 1}
+end
---
+++ test/vertical_matching_test.exs
@@ -0,0 +1,41 @@
+defmodule VerticalMatchingTest do
+  use ExUnit.Case
+  doctest VerticalMatching
+
+  describe "contiguous vertical moves" do
+    test "is zero if there are no coins" do
+      start = {}
+      coins = []
+      assert VerticalMatching.contiguous_moves(start, coins, :positive) == 0
+      assert VerticalMatching.contiguous_moves(start, coins, :negative) == 0
+    end
+
+    test "is zero if there is no coin on the start position" do
+      start = {2, 2}
+      coins = [{0, 0}]
+      assert VerticalMatching.contiguous_moves(start, coins, :positive) == 0
+      assert VerticalMatching.contiguous_moves(start, coins, :negative) == 0
+    end
+
+    test "is one if there is one move at the start position" do
+      start = {2, 2}
+      coins = [{0, 0}, {2, 2}]
+      assert VerticalMatching.contiguous_moves(start, coins, :positive) == 1
+      assert VerticalMatching.contiguous_moves(start, coins, :negative) == 1
+    end
+
+    test "is three if there are three contiguous moves in a positive direction" do
+      start = {2, 2}
+      coins = [{2, 2}, {2, 3}, {2, 4}]
+      assert VerticalMatching.contiguous_moves(start, coins, :positive) == 3
+      assert VerticalMatching.contiguous_moves(start, coins, :negative) == 1
+    end
+
+    test "is three if there are three contiguous moves in a negative direction" do
+      start = {2, 2}
+      coins = [{2, 2}, {2, 1}, {2, 0}]
+      assert VerticalMatching.contiguous_moves(start, coins, :positive) == 1
+      assert VerticalMatching.contiguous_moves(start, coins, :negative) == 3
+    end
+  end
+end
